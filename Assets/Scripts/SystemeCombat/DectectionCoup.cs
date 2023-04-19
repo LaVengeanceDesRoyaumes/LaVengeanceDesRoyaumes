499b91e981d33f171eab2b7bfe97a46bf6525b39
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-04-19 11:38:07 -0400

    Commencement de la détection de coups

--- Assets/Scripts/SystemeCombat/DectectionCoup.cs
+++ Assets/Scripts/SystemeCombat/DectectionCoup.cs
@@ -4,40 +4,65 @@ using UnityEngine;
 
 public class DectectionCoup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float pointsDeVie = 100f; // points de vie du personnage
+    public float degats = 10; // les dégâts infligés aux ennemis
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
     }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "ennemi")
+        {
+            print("Vous avez frappé l'ennemi ! Il lui reste " + pointsDeVie + " points de vie.");
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                pointsDeVie -= degats; // soustraire les dégâts infligés aux points de vie du personnage
+                DonnerCoup();
+            }
+        }
+    }
+
+    // Fonction pour infliger des dégâts aux ennemis
+    private void DonnerCoup()
+    {
+        degats = 10;
+    }
+
+}
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DectectionCoup : MonoBehaviour
+{
+    public float pointsDeVie = 100f; // point de vie du personnage
+    public float degats = 10; // les dégâts infligés aux ennemis
+
+    // Update is called once per frame
+
+
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "ennemi")
         {
-            print("ENTER");
+            print("Vous avez frappé l'ennemi ! Il lui reste " + pointsDeVie + " points de vie.");
+
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                DonnerCoup();
+            }
+
         }
     }
 
-    void OnCollisionStay(Collision other)
+    private void DonnerCoup()
     {
-        if (other.gameObject.tag == "ennemi")
-        {
-            print("STAY");
-        }
+        degats = 10;
     }
+}*/
 
-    void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.tag == "ennemi")
-        {
-            print("EXIT");
-        }
-
-    }
-}
 
